147efef3c24c12967a4039b1ad354618fbc22a3d
Shrikanth-p <shrikanthchoudary05@gmail.com>
2022-10-07 17:43:10 +0530

    Add project files.

---
+++ files/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+namespace table
+{
+    class tables
+    {
+        public static void Main()
+        {
+           
+            string path = @"Tables.txt";
+            if (!File.Exists(path))
+            {
+               
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Tables From 1 to 10 : ");
+                    for (int i = 1; i < 11; i++)
+                    {
+                        for (int j = 1; j < 11; j++)
+                        {
+                            sw.WriteLine($"{i}x{j}={i * j}");
+                        }
+                        sw.WriteLine();
+                    }
+                    
+                }
+
+            }
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+        }
+    }
+}
